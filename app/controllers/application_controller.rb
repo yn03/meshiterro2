9266293c86fdac6601fd2219e31714da8a09d829
yn03 <mlkn862n@gmail.com>
2022-11-08 17:41:27 +0000

    ユーザー認証　

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+ before_action :configure_permitted_parameters, if: :devise_controller?
+
+  #サインイン後の遷移先の設定
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  #サインアウト後の遷移先の設定
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
