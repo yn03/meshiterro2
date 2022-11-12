b52850645404ccb6faffedbefaa49d3397f8d565
yn03 <mlkn862n@gmail.com>
2022-11-12 05:45:12 +0000

    画像投稿,削除機能　

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -12,13 +12,17 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.page(params[:page])
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image =  PostImage.find(params[:id])
   end
 
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
 
   # 投稿データのストロングパラメータ
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,11 +3,10 @@ class PostImage < ApplicationRecord
   belongs_to :user
 
   def get_image
-    if image.attached?
+    unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    else
+    end
       image
     end
-  end
 end
