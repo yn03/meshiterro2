53a589d4a69157ff5789a4a17de7edfafe1e5c26
yn03 <mlkn862n@gmail.com>
2022-11-15 13:43:27 +0000

    コメント削除機能　

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   #ネストする
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resources :post_comments, only: [:create]
+    resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
